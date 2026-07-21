04cf72fc865ede173a8f5d10a8987a9d529ad0db
Favian Hilmi <favianhilmi11@gmail.com>
2026-07-21 21:39:04 +0700

    chore: setup initial Laravel project and PostgreSQL configuration

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TaskController;
+use Illuminate\Support\Facades\Route;
+
+// public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('tasks', TaskController::class);
+});
